6e23afc400253a33127a9c23fd7fde852765dd36
jayant sarkar <codejayant@gmail.com>
2017-10-12 00:07:10 -0700

    indentation updated to spaces 2

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const express = require('express')
 var app = express()
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express</h1>')
-    res.send({
-        name: 'Jayant',
-        likes: [
-            'chocolate',
-            'chips'
-        ]
-    })
+  // res.send('<h1>Hello Express</h1>')
+  res.send({
+    name: 'Jayant',
+    likes: [
+      'chocolate',
+      'chips'
+    ]
+  })
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>')
+  res.send('<h1>About Page</h1>')
 })
 
 app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'bad file'
-    })
+  res.send({
+    errorMessage: 'bad file'
+  })
 })
 
 app.listen(3000)
