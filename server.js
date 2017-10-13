fda3e34d1bc52f8eb7501250770c018bc77ebbbd
jayant sarkar <codejayant@gmail.com>
2017-10-12 23:56:23 -0700

    handlebars partials example

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const hbs = require('hbs')
 
 var app = express()
 
-app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')  // to register usage of partials. path will be absolute.
+app.set('view engine', 'hbs') // to use handlebars as view engine
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
